c6d54c43548331a04a9bea2ff3633e2883462657
likebirds <limotit28@gmail.com>
2025-01-02 13:00:56 +0300

    Update RunRDPScript.bat

--- RunRDPScript.bat
+++ RunRDPScript.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {iwr -Uri 'https://github.com/temujin-glitch/rdp/blob/a051e1adc6f7dd47eb36332502a9e1d2fd22228b/EnableRDP.ps1' -OutFile 'C:\Temp\EnableRDP.ps1'; Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File C:\Temp\EnableRDP.ps1' -Verb RunAs}"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {iwr -Uri 'https://github.com/likebirds/rdp/blob/main/EnableRDP.ps1' -OutFile 'C:\Temp\EnableRDP.ps1'; Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File C:\Temp\EnableRDP.ps1' -Verb RunAs}"
